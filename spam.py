762aced2c6fdab519ed20be89c9c8d9c83b760e5
chandra.mouli <chandra.mouli@servicenow.com>
2020-06-30 00:00:13 +0530

    Few minor changes

--- spam.py
+++ spam.py
@@ -5,7 +5,8 @@ import pandas as pd
 data = pd.read_csv('spam.csv', skiprows=1, engine='python')
 print(data.head)
 
+#Using TFID Vectorizer. 
 vectorizer = TfidfVectorizer()
 X_train = vectorizer.fit_transform(data.iloc[:, 1])
 
-print(X.shape)
+print(X_train.shape)
