75bb5ae2d612a823c4769b350333c14cc42b7940
chandra.mouli <chandra.mouli@servicenow.com>
2020-07-01 10:53:51 +0530

    remove integer mappings

--- spam.py
+++ spam.py
@@ -7,22 +7,23 @@ from sklearn.model_selection import train_test_split
 import pandas as pd 
 import numpy as np
 
+#preprocessing the data.
 
 df = pd.read_csv('spam.csv', engine = 'python')
 df = df.drop(["Unnamed: 2", "Unnamed: 3", "Unnamed: 4"],axis =1)
 mydata = df[["v1", "v2"]]
-mydata['labels'] = mydata['v1'].map({'ham' : 0, 'spam' : 1})
+#mydata['labels'] = mydata['v1'].map({'ham' : 0, 'spam' : 1})
+mydata['labels'] = mydata['v1']
+
 mydata['v2'] = mydata['v2'].str.lower()
 mydata = mydata.drop(['v1'], axis=1)
 mydata = mydata.drop(mydata.index[0])
-#print(mydata.head(2))
 
-#print(mydata.shape)
 
 
 #Using TFID Vectorizer. 
-#vectorizer = CountVectorizer(decode_error='ignore')
-vectorizer = TfidfVectorizer(decode_error='ignore')
+vectorizer = CountVectorizer(decode_error='ignore')
+#vectorizer = TfidfVectorizer(decode_error='ignore')
 X_train_counts = vectorizer.fit_transform(mydata['v2'])
 
 X_train, X_test, y_train, y_test = train_test_split(X_train_counts, mydata['labels'], random_state = 0)
@@ -33,6 +34,24 @@ clf = MultinomialNB().fit(X_train, y_train)
 print(X_train.shape)
 
 
+#predictions
 
 predicted = clf.predict(X_test)
 print(np.mean(predicted == y_test))
+
+mydata['pred'] = clf.predict(X_train_counts)
+print(mydata.head(1))
+correct_spam = mydata[(mydata['pred'] == 'ham') & (mydata['labels'] == 'spam')]['v2']
+print("Size " , correct_spam.size)
+for text in correct_spam:
+  print(text)
+
+print("********************************")
+
+wrong_spam = mydata[(mydata['pred'] == 'spam') & (mydata['labels'] == 'ham')]['v2']
+print("Size " , wrong_spam.size)
+for text in wrong_spam:
+  print(text)
+
+text = 'Hello i would like to get on a ride with you , whatss up'
+print (clf.predict())
